a1dc2cf9a27a00a4fd2f80fbd96ddf2310c80b72
harrishuang <huangjinping1000@163.com>
2016-12-06 10:58:45 +0800

    新的文件

---
+++ app/src/main/java/com/de/hjp/recyclerview/NickActivity.java
@@ -0,0 +1,19 @@
+package com.de.hjp.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ * 黄金平
+ * Created by harrishuang on 2016/12/6.
+ */
+
+public class NickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+
+}
