f3559fa4cd1d53ad200ecd95ffa996775adef52b
harrishuang <huangjinping1000@163.com>
2016-12-06 18:40:45 +0800

    新的文件

--- app/src/main/java/com/de/hjp/recyclerview/Test.java
+++ app/src/main/java/com/de/hjp/recyclerview/Test.java
@@ -1,7 +1,18 @@
 package com.de.hjp.recyclerview;
 
+import android.widget.TextView;
+
 /**
  * Created by harrishuang on 16/4/17.
  */
 public class Test {
+
+    private TextView view;
+    public Test(TextView view) {
+        this.view = view;
+    }
+
+    public TextView getView() {
+        return view;
+    }
 }
