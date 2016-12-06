3381ed1b15025cbb9629b22d4ace2371670de15d
harrishuang <huangjinping1000@163.com>
2016-12-06 18:44:18 +0800

    新的文件

---
+++ app/src/main/java/com/de/hjp/recyclerview/WoDecodeActivity.java
@@ -0,0 +1,18 @@
+package com.de.hjp.recyclerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+/**
+ * Created by harrishuang on 2016/12/6.
+ */
+
+public class WoDecodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new LinearLayout(this));
+    }
+}
